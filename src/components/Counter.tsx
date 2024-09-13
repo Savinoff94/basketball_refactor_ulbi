ef2d2f9afea6fb291326d19c19757fe99d8a2262
Savinoff94 <andreisavinovwork@gmail.com>
2024-09-13 03:28:02 +0300

    react and scss

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import './Counter.scss'
+
+
+export const Counter = () => {
+
+    const [counter,setCounter] = useState(0)
+
+    const increment = () => setCounter(counter + 1)
+
+    return (
+        <div>
+            {counter}
+            <button onClick={increment}>increment</button>
+        </div>
+    )
+}
