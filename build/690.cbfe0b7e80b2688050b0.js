200a343fb5a4ea3238c084188c2d7e58e3afaa79
Savinoff94 <andreisavinovwork@gmail.com>
2024-09-13 23:58:02 +0300

    routing, pages lazy loading

---
+++ build/690.cbfe0b7e80b2688050b0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbasketball_ulbi_refactor=self.webpackChunkbasketball_ulbi_refactor||[]).push([[690],{690:(e,a,l)=>{l.r(a),l.d(a,{default:()=>t});var r=l(848);const t=function(){return(0,r.jsx)("div",{children:"Main"})}}}]);
---
+++ build/872.825af75d24bba5ca3e5f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbasketball_ulbi_refactor=self.webpackChunkbasketball_ulbi_refactor||[]).push([[872],{872:(e,t,a)=>{a.r(t),a.d(t,{default:()=>r});var l=a(848);const r=function(){return(0,l.jsx)("div",{children:"About"})}}}]);
