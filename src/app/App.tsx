155d54933adf2a5e1bbe0ff62703792f25713b8f
Savinoff94 <andreisavinovwork@gmail.com>
2024-09-20 18:10:10 +0300

    imports, feachure sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
-import '../styles/index.scss'
+import './styles/index.scss'
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Suspense } from 'react';
-import { AboutPageAsync } from '../pages/AboutPage/AboutPageAsync';
-import { MainPageAsync } from '../pages/MainPage/MainPageAsync';
-import { useTheme } from '../Theme/useTheme';
-import { classNames } from '../helpers/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { useTheme } from './providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 
 const App = () => {
@@ -20,8 +20,8 @@ const App = () => {
             <Link to={'about'}>AboutPage</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path='/' element={<MainPageAsync/>}/>
-                    <Route path='/about' element={<AboutPageAsync/>}/>
+                    <Route path='/' element={<MainPage/>}/>
+                    <Route path='/about' element={<AboutPage/>}/>
                 </Routes>
             </Suspense>
         </div>
